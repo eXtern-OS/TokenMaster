ff485daaa1b60c7bb34594ecfbd04714d61d0194
foxclore <foxclore@zoho.com>
2020-05-23 12:06:22 +0300

    Update commit

--- db.go
+++ db.go
@@ -75,3 +75,28 @@ func RemoveToken(id string) {
 	}
 	return
 }
+
+func PutToken(token Token) int {
+	client, err := mongo.NewClient(options.Client().ApplyURI(URI))
+	if err != nil {
+		log.Println(err)
+		go beatrix.SendError("Error creating new mongo client", "PUTTOKEN")
+		return http.StatusInternalServerError
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Println(err)
+		go beatrix.SendError("Error connecting with new mongo client", "PUTTOKEN")
+		return http.StatusInternalServerError
+	}
+
+	collection := client.Database("Users").Collection("token")
+	_, err = collection.InsertOne(context.Background(), token)
+	if err != nil {
+		log.Println(err)
+		go beatrix.SendError("Error inserting token", "PUTTOKEN")
+		return http.StatusInternalServerError
+	}
+	return http.StatusOK
+}
